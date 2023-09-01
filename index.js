def31e4ffd838d7cecb7331373295009dd57274a
hiramatsuyuusuke <140389350+hiramatsuyuusuke@users.noreply.github.com>
2023-09-01 13:49:03 +0900

    commit24

--- index.js
+++ index.js
@@ -4,18 +4,41 @@ $(function(){
     /***** ページTOPに移動 *****/
     $("#back-top a").hide();                          /*見えないように隠す*/ 
     $("#back-top a").css("visibility", "visible");    /*隠した後に非表示設定を解除する*/ 
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 200) {
-            $('#back-top a').fadeIn();
-        } 
-        else {
-            $('#back-top a').fadeOut();
+
+    $(window).scroll(
+        function()
+        {
+            if ($(this).scrollTop() > 200)
+            {
+                $('#back-top a').fadeIn();
+            } 
+            else {
+                $('#back-top a').fadeOut();
+            }
         }
-    });
-    $("#back-top a").click(function(){
-        $("body,html").animate({scrollTop: 0}, 500);
-        return false;
-    });
+    );
+    $("#back-top a").click(
+        function()
+        {
+            $("body,html").animate({scrollTop: 0}, 500);
+            return false;
+        }
+    );
+
+
+    /******Homeのドロップダウン******/
+    $(".Home_drop_sub").hide();                          /*見えないように隠す*/ 
+    $(".Home_drop_sub").css("visibility", "visible");    /*隠した後に非表示設定を解除する*/ 
+
+    $(".Home_drop").hover(
+        function() {
+            $(".Home_drop_sub:not(:animated)", this).fadeIn();
+        },
+        function() {
+            $(".Home_drop_sub:not(:animated)", this).fadeOut();
+        }
+    );
+
 
 })
 
