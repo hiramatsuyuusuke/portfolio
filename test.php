357ab24a535f8ae04aca114598143bf1c29d941a
hiramatsuyuusuke <140389350+hiramatsuyuusuke@users.noreply.github.com>
2023-09-06 21:19:01 +0900

    commit37

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>データベース接続</title>
+    <style>
+        table,td,th {
+            border: 1px solid #000;
+            padding: 5px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+
+    <?php echo "myForm"; ?>
+</body>
+</html>
